0ddcd16b95b9b82ac684b7c00c24e35b5da65dbb
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2025-06-25 18:00:36 -0600

    P03 - Validaciones y mejora de IU

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Calculadora } from "./calculadora/calculadora";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
